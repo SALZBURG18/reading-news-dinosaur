ac0304705faa32f55ba8e81f47cfece2ca274d5f
SALZBURG18 <lethinkg7894@gmail.com>
2024-12-08 23:25:48 +0700

    feat(file): add main, header, footer, home files

---
+++ src/index.js
@@ -0,0 +1,28 @@
+const express    = require('express');
+const morgan     = require('morgan');
+const path       = require('path');
+const handlebars = require('express-handlebars');
+const app        = express();
+const PORT       = 3000;
+
+// HTTP combie
+// app.use(morgan('combined'));
+
+// Template engine
+app.engine('hbs', handlebars.engine({
+    extname: '.hbs'
+  }));
+  app.set('view engine', 'hbs');
+  app.set('views', path.join(__dirname, 'resources/views'));
+
+// Routes
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+// Start Server
+app.listen(PORT, () => {
+    console.log(
+        `Example app listening on port ${PORT} at http://localhost:${PORT}`
+    );
+});
